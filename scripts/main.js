e65bb7fee37b7fc319dd5ba41fb1fe9893e8aa97
RangerMauve <RangerMauve@hotmail.com>
2014-05-17 12:03:04 -0400

    Changed controls to move player diagonally

--- scripts/main.js
+++ scripts/main.js
@@ -26,8 +26,8 @@ function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	var colorchange = 64 * Math.sin((Date.now() % 1000) / 1000 * Math.PI)
 	var z = Math.sin((Date.now() % 2000) / 2000 * Math.PI);
-	var floor = Shape.Prism(Point.ORIGIN, world.width, world.height, 2 + z).translate(-player.x,-player.y,0);
-	var player_shape = Shape.Prism(new Point(player.x, player.y, player.z + z), 1, 1, 1).translate(-player.x,-player.y,0);;
+	var floor = Shape.Prism(Point.ORIGIN, world.width, world.height, 2 + z).translate(-player.x, -player.y, 0);
+	var player_shape = Shape.Prism(new Point(player.x, player.y, player.z + z), 1, 1, 1).translate(-player.x, -player.y, 0);;
 	var color = new Color(50 + colorchange, 60, 160);
 	iso.add(floor, color);
 	iso.add(player_shape, color);
@@ -40,12 +40,20 @@ document.onkeydown = function (e) {
 	var event = window.event ? window.event : e;
 	var key = event.keyCode;
 	if (key === 37) {
-		if (player.x > 0) player.x -= 1;
+		player.x -= 1;
+		player.y+=1;
 	} else if (key === 39) {
-		if (player.x < world.width-1) player.x += 1;
+		player.x += 1;
+		player.y-=1;
 	} else if (key === 38) {
-		if (player.y < world.height-1) player.y += 1;
+		player.y += 1;
+		player.x += 1;
 	} else if (key === 40) {
-		if (player.y > 0) player.y -= 1;
+		player.y -= 1;
+		player.x -= 1;
 	}
+	if (player.y < 0) player.y = 0;
+	if (player.y > world.height - 1) player.y = world.height - 1;
+	if (player.x < 0) player.x = 0;
+	if (player.x > world.width - 1) player.x = world.width - 1;
 }
